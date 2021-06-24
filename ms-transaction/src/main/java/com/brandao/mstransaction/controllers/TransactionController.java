245cd07a5500f5ad1f76eaecda7ad828db3bcbbf
tauabrandao <brandao.taua@gmail.com>
2021-06-24 19:57:20 -0300

    [ms-transaction] open api and swagger ui

--- ms-transaction/src/main/java/com/brandao/mstransaction/controllers/TransactionController.java
+++ ms-transaction/src/main/java/com/brandao/mstransaction/controllers/TransactionController.java
@@ -3,6 +3,8 @@ package com.brandao.mstransaction.controllers;
 import com.brandao.mstransaction.dto.Order;
 import com.brandao.mstransaction.dto.StockDTO;
 import com.brandao.mstransaction.services.TransactionService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
 
+@Tag(name = "Transaction controller")
 @RestController
 @RequestMapping("/transaction")
 public class TransactionController {
@@ -22,6 +25,7 @@ public class TransactionController {
         this.service = service;
     }
 
+    @Operation(summary = "Get resumed order by a stock code and its amount")
     @GetMapping("/calculate/{amount}/{stock}")
     public ResponseEntity<Object> resumeOrder(@PathVariable Long amount, @PathVariable String stock) {
         Order order = service.resumeOrder(stock, amount);
